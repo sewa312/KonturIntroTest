b1b0c829af88af36e7b86958b3d6b7714552eb8a
sewa312 <sewa080@mail.ru>
2016-11-18 01:49:23 +0300

    SygnalParse added

--- SignalParse/Program.cs
+++ SignalParse/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,34 @@ namespace SignalParse
     {
         static void Main(string[] args)
         {
+            ///GAMA_stop_word.scc429
 
-            int i = -14;
-            byte b = (byte)(i & 0x01);
-            Console.WriteLine(b);
+            Signal signal = new Signal(File.Open(args[0], FileMode.Open));
+            
+
+            Console.WriteLine("| Время       | Адрес | Данные                     | Матрица | Контроль |");
+            foreach (Block block in signal.Read())
+            {
+                string data = Convert.ToString(block.Data, 2).PadLeft(21, '0');
+                var outputData = new StringBuilder()
+                    .Append(data.Substring(0, 1));
+                for (int i = 1; i < 21; i += 4)
+                {
+                    outputData.Append(' ').Append(data.Substring(i, 4));
+                }
+
+                Console.WriteLine("|{0,12:F2} |  {1} | {2} |       {3} |        {4} |", 
+                    block.Time / 100.0,
+                    Convert.ToString(block.Address, 8).PadLeft(4, '0'),
+                    outputData,
+                    block.Matrix,
+                    block.Control);
+                Console.Write("Press Q to exit or any key to continue... ");
+                if (Console.ReadKey().KeyChar == 'q')
+                    break;
+                Console.CursorLeft = 0;
+            }
+
             Console.ReadLine();
         }
     }
--- SignalParse/Signal.cs
+++ SignalParse/Signal.cs
@@ -28,41 +28,53 @@ namespace SignalParse
     class Signal
     {
         private const long HeaderSize = 1024;
-        private const long BlockSize = 8;
+        private const long BlockSize = 64;
 
         private Stream stream;
-        List<Block> list;
 
         public Signal(Stream input)
         {
             stream = input;
-            list = new List<Block>();
-            foreach (Block b in Read())
-            {
-                list.Add(b);
-            }
         }
 
         public IEnumerable<Block> Read()
         {
             stream.Seek(HeaderSize, SeekOrigin.Begin);
             long totalSize = stream.Length;
+            //var reader = new BinaryReader(File.Open("GAMA_stop_word.scc429", FileMode.OpenOrCreate));
             var reader = new BinaryReader(stream, Encoding.UTF8, leaveOpen: true);
+
+            //header = reader.ReadChars(128);
+
             while (stream.Position + BlockSize < totalSize)
             {
                 int time = reader.ReadInt32();
-                int block = reader.ReadInt32();
-
+                if (!BitConverter.IsLittleEndian)
+                    time = ReverseByteOrder(time);
+                
+                uint block = reader.ReadUInt32();
+                if (BitConverter.IsLittleEndian)
+                    block = ReverseByteOrder(block);
+                
                 byte address = (byte)(block >> 24);
                 uint data = (uint)(block & 0x00FFFFFF);
                 data = data >> 3;
-                byte matrix = (byte)(block & 0x03);
+                byte matrix = (byte)(block & 0x000007);
                 matrix = (byte)(matrix >> 1);
                 byte control = (byte)(block & 0x01);
 
                 yield return new Block(time, address, data, matrix, control);
             }
+        }
 
+        private UInt32 ReverseByteOrder(UInt32 source)
+        {
+            return (source << 24) | ((source & 0x0000FF00) << 8) | ((source & 0x00FF0000) >> 8) | (source >> 24);
+        }
+
+        private Int32 ReverseByteOrder(Int32 source)
+        {
+            return (Int32)ReverseByteOrder((UInt32)source);
         }
     }
 }
